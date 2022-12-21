2e26d24fdfa686a5f8df9932de0c6ba2c1b73a38
saahn-okestro <sa.ahn@oksetro.com>
2022-12-21 16:18:03 +0900

    monitoring commit

--- log/log.py
+++ log/log.py
@@ -6,23 +6,11 @@ import sys
 
 
 class standardLog:
-    """
-    This standardLog class is base of making logs
-    """
     def __init__(self):
-        """
-        This is logs init function
-
-        Arguments:
-            name -- operate function`s file path
-            file_name -- operate function`s file name
-        """
         self.name_path = os.path.abspath(__file__)
         self.file_path = sys._getframe(1).f_globals['__file__'].split('/')[-1].split('.')[0]
         self.project_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
         self.logger = logging.getLogger(self.name_path)
-        # self.name = name
-        # self.file_name = file_name
         self.logger.setLevel(logging.INFO)
         self.streamHandler = logging.StreamHandler()
         self.totalHandler = logging.FileHandler(self.project_path + '/utils/logs/total.log', mode='a')
@@ -36,9 +24,6 @@ class standardLog:
         self.func_name = sys._getframe(2).f_code.co_name
 
     def log_handle(self):
-        """
-        This is logs common part
-        """
         self.streamHandler.setFormatter(self.formatter)
         self.totalHandler.setFormatter(self.formatter)
         self.fileHandler.setFormatter(self.formatter)
@@ -48,3 +33,48 @@ class standardLog:
         self.logger.addHandler(self.streamHandler)
         self.logger.addHandler(self.fileHandler)
         self.logger.addHandler(self.totalHandler)
+
+    def sending_log(self, level, e=None):
+        self.set_filename()
+        self.logger = logging.getLogger(self.name_path)
+        extra = {'context_message': self.file_name,
+                 'context_message2': self.func_name}
+        self.logger = logging.getLogger(self.name_path)
+
+        if level == 'success':
+            context = 'Successed !'
+            self.formatter = logging.Formatter(
+                '[Time: %(asctime)s] '
+                '[Level: %(levelname)s] '
+                '[MainPipeline: %(context_message)s] '
+                '[FunctionFile: %(pathname)s] '
+                '[Fline: %(lineno)d] '
+                f'[Context: The "%(context_message2)s" is {context}] '
+                f'[Reason: %(message)s]')
+
+        elif level == 'warning':
+            context = 'Warning !!'
+            self.formatter = logging.Formatter(
+                '[Time: %(asctime)s] '
+                '[Level: %(levelname)s] '
+                '[MainPipeline: %(context_message)s] '
+                '[FunctionFile: %(pathname)s] '
+                '[Fline: %(lineno)d] '
+                f'[Context: The "%(context_message2)s" is {context}] '
+                f'[Reason: %(message)s]')
+
+        elif level == 'error':
+            context = 'Failed !!!'
+            self.formatter = logging.Formatter(
+                '[Time: %(asctime)s] '
+                '[Level: %(levelname)s] '
+                '[MainPipeline: %(context_message)s] '
+                '[FunctionFile: %(pathname)s] '
+                '[Fline: %(lineno)d] '
+                f'[Context: The "%(context_message2)s" is {context}] '
+                f'[Reason: %(message)s] '
+                f'[Error Type: {type(e)}]')
+
+        self.log_handle()
+        self.logger = logging.LoggerAdapter(self.logger, extra)
+        return self.logger
