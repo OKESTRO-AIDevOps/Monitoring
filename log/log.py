9fa13e5a08510a9a2e9a9bed0759b891e2bbc33f
gjpark-okestro <gj.park@okestro.com>
2023-10-04 19:57:37 +0900

    monitoring commit

--- log/log.py
+++ log/log.py
@@ -1,38 +1,35 @@
 import logging
-import time
 import os
 import sys
 
-
-
-class standardLog:
+class StandardLog:
     def __init__(self):
         self.name_path = os.path.abspath(__file__)
-        self.file_path = sys._getframe(1).f_globals['__file__'].split('/')[-1].split('.')[0]
+        self.file_path = os.path.splitext(os.path.basename(sys._getframe(1).f_globals['__file__']))[0]
         self.project_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
         self.logger = logging.getLogger(self.name_path)
         self.logger.setLevel(logging.INFO)
-        self.streamHandler = logging.StreamHandler()
-        self.totalHandler = logging.FileHandler(self.project_path + '/utils/logs/total.log', mode='a')
-        self.fileHandler = logging.FileHandler(self.project_path + '/utils/logs/' + self.file_path + '.log', mode='a')
+        self.stream_handler = logging.StreamHandler()
+        self.total_handler = logging.FileHandler(os.path.join(self.project_path, 'utils', 'logs', 'total.log'), mode='a')
+        self.file_handler = logging.FileHandler(os.path.join(self.project_path, 'utils', 'logs', f'{self.file_path}.log'), mode='a')
         self.formatter = None
         self.file_name = None
-        self.func_anme = None
+        self.func_name = None
 
     def set_filename(self):
         self.file_name = sys._getframe(3).f_globals['__file__']
         self.func_name = sys._getframe(2).f_code.co_name
 
     def log_handle(self):
-        self.streamHandler.setFormatter(self.formatter)
-        self.totalHandler.setFormatter(self.formatter)
-        self.fileHandler.setFormatter(self.formatter)
-        if type(self.logger) is logging.LoggerAdapter:
+        self.stream_handler.setFormatter(self.formatter)
+        self.total_handler.setFormatter(self.formatter)
+        self.file_handler.setFormatter(self.formatter)
+        if isinstance(self.logger, logging.LoggerAdapter):
             return
         self.formatter.converter = time.gmtime
-        self.logger.addHandler(self.streamHandler)
-        self.logger.addHandler(self.fileHandler)
-        self.logger.addHandler(self.totalHandler)
+        self.logger.addHandler(self.stream_handler)
+        self.logger.addHandler(self.file_handler)
+        self.logger.addHandler(self.total_handler)
 
     def sending_log(self, level, e=None):
         self.set_filename()
@@ -42,38 +39,24 @@ class standardLog:
         self.logger = logging.getLogger(self.name_path)
 
         if level == 'success':
-            context = 'Successed !'
-            self.formatter = logging.Formatter(
-                '[Time: %(asctime)s] '
-                '[Level: %(levelname)s] '
-                '[MainPipeline: %(context_message)s] '
-                '[FunctionFile: %(pathname)s] '
-                '[Fline: %(lineno)d] '
-                f'[Context: The "%(context_message2)s" is {context}] '
-                f'[Reason: %(message)s]')
-
+            context = 'Succeeded!'
         elif level == 'warning':
-            context = 'Warning !!'
-            self.formatter = logging.Formatter(
-                '[Time: %(asctime)s] '
-                '[Level: %(levelname)s] '
-                '[MainPipeline: %(context_message)s] '
-                '[FunctionFile: %(pathname)s] '
-                '[Fline: %(lineno)d] '
-                f'[Context: The "%(context_message2)s" is {context}] '
-                f'[Reason: %(message)s]')
-
+            context = 'Warning!'
         elif level == 'error':
-            context = 'Failed !!!'
-            self.formatter = logging.Formatter(
-                '[Time: %(asctime)s] '
-                '[Level: %(levelname)s] '
-                '[MainPipeline: %(context_message)s] '
-                '[FunctionFile: %(pathname)s] '
-                '[Fline: %(lineno)d] '
-                f'[Context: The "%(context_message2)s" is {context}] '
-                f'[Reason: %(message)s] '
-                f'[Error Type: {type(e)}]')
+            context = 'Failed!'
+        else:
+            raise ValueError(f"Invalid log level: {level}")
+
+        self.formatter = logging.Formatter(
+            '[Time: %(asctime)s] '
+            '[Level: %(levelname)s] '
+            '[MainPipeline: %(context_message)s] '
+            '[FunctionFile: %(pathname)s] '
+            '[Fline: %(lineno)d] '
+            f'[Context: The "{self.func_name}" is {context}] '
+            f'[Reason: %(message)s] '
+            f'[Error Type: {type(e)}]' if level == 'error' else f'[Context: {context}]'
+        )
 
         self.log_handle()
         self.logger = logging.LoggerAdapter(self.logger, extra)
--- utils/base.py
+++ utils/base.py
@@ -2,50 +2,51 @@ import os
 import sys
 import logging
 from configparser import ConfigParser, NoSectionError
-
 from elasticsearch import Elasticsearch
 
-project_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-logs_path =  project_path + "/utils/logs"
-log_file = logs_path + "/es.log"
-
-if not os.path.exists(logs_path):
-    os.mkdir(logs_path)
-    f = open(log_file, 'a')
-    f.write('')
-logging.basicConfig(filename=log_file, level=logging.INFO)
-
-sys.path.append(project_path)
 
 class ESBase:
     def __init__(self):
         self.project_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        self.config_path = self.project_path + '/config.ini'
+        self.config_path = os.path.join(self.project_path, 'config.ini')
         self.config = self.read_config(self.config_path)
-
 
     def read_config(self, config_path):
         config = ConfigParser()
         config.read(config_path)
         return config
 
-
     def connect(self, config):
         try:
-            es_host = config.get('ES','HOST')
-            es_port = config.get('ES','PORT')
-            es_id = config.get('ES','USER')
-            es_pw = config.get('ES','PASSWORD')
+            es_host = config.get('ES', 'HOST')
+            es_port = config.getint('ES', 'PORT')  # Assume PORT is an integer
+            es_id = config.get('ES', 'USER')
+            es_pw = config.get('ES', 'PASSWORD')
 
             es = Elasticsearch(hosts=f"http://{es_id}:{es_pw}@{es_host}:{es_port}/", timeout=1000)
-            
+
             if not es.ping():
-                logging.error('ES Connetion Error')
-                raise(Exception(f"ES Connetion Error"))
-            
+                logging.error('ES Connection Error')
+                raise Exception("ES Connection Error")
+
             return es
 
         except NoSectionError:
-            logging.error(f'Fail to find ES config')
+            logging.error('Failed to find ES config')
 
 
+if __name__ == "__main__":
+    project_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    logs_path = os.path.join(project_path, "utils", "logs")
+    log_file = os.path.join(logs_path, "es.log")
+
+    if not os.path.exists(logs_path):
+        os.makedirs(logs_path)
+        with open(log_file, 'a'):
+            pass
+
+    logging.basicConfig(filename=log_file, level=logging.INFO)
+
+    es_base = ESBase()
+    es_config = es_base.config
+    es_connection = es_base.connect(es_config)
